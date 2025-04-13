a0b5972aeb21d1ba1d2a51663280f04bb178d78f
Dhaval rathod <drcoder389@gmail.com>
2025-04-13 14:18:21 +0530

    changes

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -2,16 +2,15 @@ import React from 'react'
 
 import {Outlet} from "react-router-dom"
 
-import Header from './Header'
+
 import Footer from './Footer'
-import { useLocation } from "react-router-dom";
+
 
 
 
 
 const Layout = () => {
-  const location = useLocation();
-   console.log(location.pathname);
+
 
   return (
     
@@ -19,7 +18,7 @@ const Layout = () => {
      
     
 
-      {/* <Header/> */}
+    
      
      
       <Outlet />
